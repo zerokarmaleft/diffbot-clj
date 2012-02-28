8fb1a4753d6cfc37781ad4cf407b957eebaa1f75
Edward Cho <zerokarmaleft@gmail.com>
2012-02-28 13:36:58 -0600

    added Frontpage Request

--- src/diffbot/core.clj
+++ src/diffbot/core.clj
@@ -1,6 +1,8 @@
 (ns diffbot.core
-  (:require [clj-http.client :as client])
-  (:use clojure.pprint
+  (:require [clj-http.client :as client]
+            [clojure.xml :as xml])
+  (:use clojure.java.io
+        clojure.pprint
 	clojure.data.json))
 
 (def ^:dynamic *base-url* "http://www.diffbot.com/api/")
@@ -9,9 +11,16 @@
   (client/request
     (merge {:method method :url (str *base-url* path)} opts)))
 
+(defn read-dml [response]
+  (xml/parse (input-stream (.getBytes response "UTF-8"))))
+
 (defn analyze [url token]
   (let [path (str "article?token=" token "&url=" url)]
     (read-json (:body (api-action :get path)))))
+
+(defn front-page [url token]
+  (let [path (str "frontpage?token=" token "&url=" url)]
+    (read-dml (:body (api-action :get path)))))
 
 ;;(client/request
 ;; (merge {:method method :url (str api-base path)} opts))
